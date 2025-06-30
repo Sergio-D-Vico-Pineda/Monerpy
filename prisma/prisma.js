c742e759aea2a66bc7fd79f509c98f6d9f690825
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-06-30 14:22:46 +0200

    database conections

---
+++ prisma/prisma.js
@@ -0,0 +1,23 @@
+// Db
+import { PrismaClient } from '@prisma/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
+
+const urlenv = import.meta.env.SECRET_TURSO_DATABASE_URL;
+const authTokenEnv = import.meta.env.SECRET_TURSO_AUTH_TOKEN;
+// const urlenv = process.env.SECRET_TURSO_DATABASE_URL;
+// const authTokenEnv = process.env.SECRET_TURSO_AUTH_TOKEN;
+
+if (!urlenv || !authTokenEnv) {
+    throw new Error('Missing Turso database configuration');
+}
+
+const libsql = createClient({
+    url: urlenv,
+    authToken: authTokenEnv
+});
+
+const adapter = new PrismaLibSQL(libsql);
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
